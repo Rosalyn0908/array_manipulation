c392edd8af9c1ee3cd4ba459c202b75adc2f8272
Rosalyn Eishen <rosalynrachel76@gmail.com>
2017-09-12 17:12:15 -0500

    Update array_manipulation.js
    Added exercise - Find all common elements in three sorted arrays

--- array_manipulation.js
+++ array_manipulation.js
@@ -56,5 +56,32 @@ $(function() {
 	);
 
 
+	// -- Find all common elements in three sorted arrays --
+	var array_1 = [4, 5, 72, 1, 22, 33];
+	var array_2 = [0, 15, 22, 10];
+	var array_3 = [10, 5, 13, 40, 65];
+	$('.example_array:eq(3)').append(
+		  JSON.stringify(array_1, null, ' ') + '<br>'
+		+ JSON.stringify(array_2, null, ' ') + '<br>'
+		+ JSON.stringify(array_3, null, ' ')
+	);
+	var combined_array    = array_1.concat(array_2, array_3);
+	var elements_in_array = {};
+	var common_elements   = {};
+
+	for (var i = 0; i < combined_array.length; i++) {
+		var element =  combined_array[i].toString();
+		if ( elements_in_array[element] ) {
+			common_elements[element] = true;
+		}
+		elements_in_array[element] = true;
+	}
+
+	$('.solution_output:eq(3)').append(
+		  'ANSWER: The numbers that appear more than once in the three arrays are: '
+		+ Object.keys(common_elements)
+	);
+
+
 
 });
