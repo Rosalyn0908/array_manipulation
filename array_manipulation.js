e37627fd83250a5958c074b08e8110c219a480a5
Rosalyn Eishen <rosalynrachel76@gmail.com>
2017-09-12 17:10:25 -0500

    Update array_manipulation.js
    Added exercise - How to find duplicate number in array

--- array_manipulation.js
+++ array_manipulation.js
@@ -1,4 +1,21 @@
 $(function() {
 	var my_array;
+	
+	// -- How to find duplicate number in array --
+	my_array         = [0, 1, 2, 3, 4, 5, 6, 7, 8, 3];
+	var expected_sum = 36
+	var actual_sum   = 0;
+	$('.example_array:eq(0)').append(JSON.stringify(my_array, null, ' '));
+
+	for (var i = 0; i < my_array.length; i++) {
+		actual_sum = actual_sum + my_array[i];
+	}
+
+	var duplicate_n = actual_sum - expected_sum;
+	$('.solution_output:eq(0)').append(
+		'ANSWER: The duplicate number is ' + duplicate_n
+	);
+
+
 
 });
