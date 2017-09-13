6527337c88b78420ff8f78ddb369ad368996c758
Rosalyn Eishen <rosalynrachel76@gmail.com>
2017-09-13 09:21:42 -0500

    Update array_manipulation.js
    Added exercise - Use bubble sorting algorithm to sort the array

--- array_manipulation.js
+++ array_manipulation.js
@@ -96,4 +96,24 @@ $(function() {
 	$('.solution_output:eq(4)').append(
 		'ANSWER: The new array will look like this: ' + JSON.stringify(my_array, null, ' ')
 	);
+	
+	
+	
+	// -- Bubble sort --
+	my_array = [3, 222, 123, 10, 5, -44, 5, 72, 1, 22, 33];
+	$('.example_array:eq(5)').append(JSON.stringify(my_array, null, ' '));
+
+	for (var i = 0; i < my_array.length - 1; i++) {
+		for (var k = 0; k < my_array.length - 1 - i; k++) {
+			if ( my_array[k] >= my_array[k + 1] ) {
+				var j = my_array[k];
+				my_array[k] = my_array[k + 1];
+				my_array[k + 1] = j;
+			}
+		}
+	}
+
+	$('.solution_output:eq(5)').append(
+		'ANSWER: The sorted array will look like this: ' + JSON.stringify(my_array, null, ' ')
+	);
 });
